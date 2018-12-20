9629242541b1e6147348e2f8a7455b01f3658d55
Sebastian <spaluszkiewicz@gmail.com>
2018-12-20 13:09:33 +0100

    update project

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
            Alles Klar
+              gute 1 comit
           </p>
           <a
             className="App-link"
